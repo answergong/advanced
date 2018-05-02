921fd52366873120639f991bde89d7f993fdc52e
gongxiangzhu <gongxiangzhu.km.com>
2018-05-02 17:14:11 +0800

    'modal'

--- backend/config/main.php
+++ backend/config/main.php
@@ -29,7 +29,7 @@ return [
 //            '*' => [
 //                'class' => 'yii\i18n\PhpMessageSource',
 //                'fileMap' => [
-//                    'common' => 'common.php' //可以加多个，是yii::t里面的第一个参数名
+//                    'rbac-admin' => 'common.php' //可以加多个，是yii::t里面的第一个参数名
 //                ],
 //                'basePath' => '@common/message', //配置语言文件路径，现在采用默认的，就可以不配置这个
 //            ],
