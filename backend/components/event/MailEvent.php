5c4eec899caab7122acbd5024bd083fb3c3349f0
gongxiangzhu <gongxiangzhu.km.com>
2018-06-05 18:04:41 +0800

    event

---
+++ backend/components/event/MailEvent.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gxz
+ * Date: 2018/6/5
+ * Time: 16:37
+ */
+
+class MailEvent
+{
+
+}
---
+++ backend/controllers/SendMailController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gxz
+ * Date: 2018/6/5
+ * Time: 15:32
+ */
